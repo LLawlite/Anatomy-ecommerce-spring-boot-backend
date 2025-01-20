3600d9f3dde1cf1c650b582a3150cda229064de9
Cicada-M <shubhamindia2001@gamil.com>
2025-01-20 21:00:41 +0530

    Added GlobalExcetpionHander and Validation for Category

--- src/main/java/com/ecommerce/anatomy/model/Category.java
+++ src/main/java/com/ecommerce/anatomy/model/Category.java
@@ -4,24 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @NotBlank(message = "Category name cannot be blank")
+    @Size(min=5,message="Category name must contain atleast 5 characters")
     private String categoryName;
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-    }
 
     public String getCategoryName() {
         return categoryName;
